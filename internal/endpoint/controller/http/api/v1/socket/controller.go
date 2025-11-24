c9c212c26d352b9200cece26b4367618c9cf58af
Gladin Danill <danyagladin@gmail.com>
2025-11-24 12:57:44 +0700

    remove logs

--- internal/endpoint/controller/http/api/v1/socket/controller.go
+++ internal/endpoint/controller/http/api/v1/socket/controller.go
@@ -1,7 +1,6 @@
 package socket
 
 import (
-	"fmt"
 	"net/http"
 	"wn/internal/domain/services/socket"
 	"wn/pkg/apperror"
@@ -31,7 +30,6 @@ func NewController(
 		services: services,
 		upgrader: websocket.Upgrader{
 			CheckOrigin: func(r *http.Request) bool {
-				fmt.Println(r.Body, r.URL.Query())
 				return true
 			},
 			ReadBufferSize:  1024,
@@ -51,27 +49,22 @@ func (h *Controller) ConnectionController(api *gin.RouterGroup) {
 // createConnection - HTTP хендлер для установки вебсокет соединения
 func (h *Controller) createConnection(c *gin.Context) {
 	userID := c.Query("user_id")
-	fmt.Println(1)
 	if userID == "" {
 		_ = c.Error(apperror.NewBadRequestError("empty user_id", "bind_query"))
 		return
 	}
-	fmt.Println(1.5)
 	userId, err := uuid.Parse(userID)
 	if err != nil {
 		_ = c.Error(apperror.NewBadRequestError(err.Error(), "bind_query"))
 		return
 	}
-	fmt.Println(2)
 	conn, err := h.upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
 		_ = c.Error(err)
 		return
 	}
-	fmt.Println(3)
 	// Создаем доменное представление соединения
 	wsConn := socket.NewWSConnection(conn, userId)
-	fmt.Println(4)
 	// Передаем управление доменному сервису
 	ctx := c.Request.Context()
 	h.services.HandleConnection(ctx, wsConn)
